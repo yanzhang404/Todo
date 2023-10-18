343d9d1095e9f7ce897c1ed7b739fea696ed19a1
yanzhang404 <1543890945@qq.com>
2023-10-18 20:07:28 +0800

    todolist

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,156 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 align="center">
+      ToDo List
+    </h1>
+      <div align="center">
+          <input type="text" v-model="newTask" placeholder="请输入你的任务名称">
+          <button @click="addTask">添加</button>
+      </div>
+      
+      <div>
+        <ul>
+            <li v-for="task in tasks" :key="task.id" :class="{ completed: task.completed }">
+                <span @click="toggleCompletion(task)">{{ task.name }}</span>
+                <button @click="deleteTask(task)">删除</button>
+            </li>
+        </ul>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+import axios from 'axios';
+
+    export default{
+    
+        name: 'App',
+        data() {
+          return{
+            newTask: '',
+            tasks: []
+          }
+        },
+
+        created() {
+        this.fetchTasks();
+        },  
+
+        methods: {
+            addTask() {
+                if (this.newTask.trim()) {
+                    const task = { name: this.newTask.trim(), completed: false };
+                    axios.post('/api/tasks', task)
+                        .then(response => {
+                            if (response.data.code === 200) {
+                                this.newTask = '';
+                                this.fetchTasks();  // 重新获取任务列表
+                            } else {
+                                console.error(response.data.msg);
+                            }
+                        })
+                        .catch(error => {
+                            console.error(error);
+                        });
+                }
+            },
+            toggleCompletion(task) {
+                task.completed = !task.completed;
+                axios.put(`/api/tasks/${task.id}`, task)
+                    .then(response => {
+                        if (response.data.code !== 200) {
+                            console.error(response.data.msg);
+                        }
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
+            },
+
+            fetchTasks() {
+                axios.get("/api/tasks")
+                    .then(response => {
+                        if (response.data.code === 200) {
+                            this.tasks = response.data.data;
+                        } else {
+                            console.error(response.data.msg);
+                        }
+                        })
+                    .catch(error => {
+                    console.error(error);
+                    });
+            },
+            deleteTask(task) {
+                axios.delete(`/api/tasks/${task.id}`)
+                    .then(response => {
+                        if (response.data.code === 200) {
+                            this.fetchTasks();  // 重新获取任务列表
+                        } else {
+                            console.error(response.data.msg);
+                        }
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
+        }
+        }
+      }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+       h1{
+          font-size: 70px;
+          color:#059565;
+          position: relative;
+       }
+       body {
+            font-family: 'Arial', sans-serif;
+            background-color: #F7F9FC;
+            padding: 20px;
+        }
+
+        li {
+            color: #333;
+            cursor: pointer;
+            padding: 20px;
+            border-radius: 3px;
+            transition: background-color 0.3s ease;
+            position: relative;
+            font-weight: bolder;
+            font-size: 25px;
+           
+        }
+
+        li:hover {
+            background-color: #E4E7ED;
+        }
+
+        .completed {
+            color: #A5ACB3;
+            text-decoration: line-through;
+        }
+
+        input[type="text"], button {
+            padding: 10px;
+            border: 1px solid #D4D9E1;
+            border-radius: 4px;
+            font-size: 16px;
+            margin-right: 10px;
+        }
+
+        button {
+            background-color: hsl(171, 82%, 29%);
+            color: #FFF;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+            position: absolute;
+            
+            
+        }
+
+        button:hover {
+            background-color: #357ABD;
+        }
+    </style>
